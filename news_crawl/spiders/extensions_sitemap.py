66b80949a07d7083f38065454a09ebcb007ebfe8
pubranko <mikuras3@outlook.com>
2021-05-05 02:16:30 +0900

    産経と読売のクローラーの共通化がとりあえず完了。

--- news_crawl/spiders/extensions_sitemap.py
+++ news_crawl/spiders/extensions_sitemap.py
@@ -6,6 +6,8 @@ import pickle
 from news_crawl.items import NewsCrawlItem
 from news_crawl.models.mongo_model import MongoModel
 from news_crawl.models.crawler_controller_model import CrawlerControllerModel
+from datetime import datetime, timedelta
+from dateutil import parser
 
 
 class ExtensionsSitemapSpider(SitemapSpider):
@@ -13,16 +15,17 @@ class ExtensionsSitemapSpider(SitemapSpider):
     SitemapSpiderの機能を拡張したクラス。
     Override → __init__(),parse(),closed()
     (前提)
-    name,allowed_domains,sitemap_urls,_domain_nameの値は当クラスを継承するクラスで設定すること。
+    name, allowed_domains, sitemap_urls, _domain_name, spider_versionの値は当クラスを継承するクラスで設定すること。
     sitemap_filter()メソッドのオーバーライドも継承先のクラスで行うこと。
     '''
-    name = 'extension_sitemap'
-    allowed_domains = ['sample.com']
-    sitemap_urls = ['https://www.sample.com/sitemap.xml', ]
-    custom_settings = {
+    name: str = 'extension_sitemap'                                 # 継承先で上書き要。
+    allowed_domains: list = ['sample.com']                          # 継承先で上書き要。
+    sitemap_urls: list = ['https://www.sample.com/sitemap.xml', ]   # 継承先で上書き要。
+    custom_settings: dict = {
         'DEPTH_LIMIT': 2,
         'DEPTH_STATS_VERBOSE': True
     }
+    spider_version: float = 0.0          # spiderのバージョン。継承先で上書き要。
     # 引数の値保存
     _lastmod_recent_time: int = 0       # 直近の15分など。分単位で指定することにしよう。0は制限なしとする。
     _url_pattern: str = ''              # 指定したパターンをurlに含むもので限定(正規表現)
@@ -32,18 +35,21 @@ class ExtensionsSitemapSpider(SitemapSpider):
     # MongoDB関連
     mongo: MongoModel                   # MongoDBへの接続を行うインスタンスをspider内に保持。pipelinesで使用。
     _crawler_controller_recode: Any     # crawler_controllerコレクションから取得した対象ドメインのレコード
-    # スパイダーの挙動制御関連、固有の条法など
+    # スパイダーの挙動制御関連、固有の情報など
     _until_this_time: datetime          # sitemapで対象とするlastmodに制限をかける時間
     _crawl_start_time: datetime         # Scrapy起動時点の基準となる時間
     _crawl_start_time_iso: str          # Scrapy起動時点の基準となる時間(ISOフォーマットの文字列)
     # sitemapで最初の1件目にあるlastmod(最新の更新)を保持。pipelineで使用。
-    _domain_name = 'sample_com'         # 各種処理で使用するドメイン名の一元管理
+    _domain_name = 'sample_com'         # 各種処理で使用するドメイン名の一元管理。継承先で上書き要。
     _latest_lastmod: str
     _latest_url: str                    # 上記'latest_lastmod'のurl
-    _spider_type:str = 'SitemapSpider'  # spiderの種類。pipelineで使用。
-    _extensions_sitemap_version:float = 1.0         # 当クラスのバージョン
-    _extensions_sitemap_filter_type:str = 'base'    # spiderの拡張type  継承先のクラスで上書きすることを前提とする。
-    _extensions_sitemap_filter_version:float = 0.0  # 上記のバージョン  継承先のクラスで上書きすることを前提とする。
+    _spider_type: str = 'SitemapSpider'  # spiderの種類。pipelineで使用。
+    _extensions_sitemap_version: float = 1.0         # 当クラスのバージョン
+
+    # sitemap_urlsに複数のサイトマップを指定した場合、その数だけsitemap_filterが可動する。その際、どのサイトマップか判別できるように処理中のサイトマップと連動するカウント。
+    _sitemap_urls_count: int = 0
+    # crawler_controllerコレクションへ書き込むレコードのdomain以降のレイアウト雛形。※最上位のKeyのdomainはサイトの特性にかかわらず固定とするため。
+    _sitemap_contened: dict = {name: {},}
 
     def __init__(self, *args, **kwargs):
         super(ExtensionsSitemapSpider, self).__init__(*args, **kwargs)
@@ -54,6 +60,9 @@ class ExtensionsSitemapSpider(SitemapSpider):
             if kwargs['debug'] == 'Yes':
                 self._debug_flg = True
                 print('=== debugモード ON ===')
+                _file = open('debug_entries.txt', 'w')  # 初期化
+                _file.close()
+
         if 'term_days' in kwargs:
             if kwargs['term_days'].isdecimal():
                 self._term_days = int(kwargs['term_days'])
@@ -76,27 +85,29 @@ class ExtensionsSitemapSpider(SitemapSpider):
                              ' は前回のcrawl情報がありません。初回から"continued"の使用は不可です。')
                 else:
                     self._continued = True
+                    #self._sitemap_contened: dict = { self.name : '', }
             else:
                 sys.exit('引数エラー：continuedに使用できるのは、"Yes"のみです。')
         # 関連チェック
         if 'lastmod_recent_time' in kwargs and 'continued' in kwargs:
             sys.exit('引数エラー：lastmod_recent_timeとcontinuedは同時には使えません。')
 
+        print('=== ExtensionsSitemapSpider の __init__終了')
+
     def parse(self, response):
         '''
         取得したレスポンスよりDBへ書き込み
         '''
-        response_time = datetime.now()
+        _info = self.name + ':' + str(self.spider_version) + ' / ' \
+            + 'extensions_sitemap:' + str(self._extensions_sitemap_version)
+
         yield NewsCrawlItem(
             url=response.url,
-            response_time=response_time,
+            response_time=datetime.now().astimezone(self.settings['TIMEZONE']),
             response_headers=pickle.dumps(response.headers),
             response_body=pickle.dumps(response.body),
-            spider_type=self._spider_type,
-            extensions_sitemap_filter_type = self._extensions_sitemap_filter_version,
-            spider_version='extensions_sitemap:' + str(self._extensions_sitemap_version) + '/filter:' + str(self._extensions_sitemap_filter_version),
+            spider_version_info=_info
         )  # ログに不要な値を出さないようitems.pyで細工して文字列を短縮。
-
 
     def closed(self, spider):
         '''
@@ -109,13 +120,40 @@ class ExtensionsSitemapSpider(SitemapSpider):
             crawler_controller.update(
                 {'domain': self._domain_name},
                 {'domain': self._domain_name,
-                 'spider_name': {
-                     self.name: {
-                         'latest_lastmod': self._latest_lastmod,
-                         'latest_url': self._latest_url,
-                         'crawl_start_time': self._crawl_start_time_iso
-                     }
-                 }}
+                self.name:self._sitemap_contened[self.name],
+                },
             )
 
         self.mongo.close()
+
+    def sitemap_filter_common_prosses(self, entries):
+        ''' 独自に拡張。
+        １．起動時の引数に応じた処理を行うため、以下の抽出に使う変数を求める。
+          => self._crawl_start_time, self._crawl_start_time_iso, self._until_this_time, self._tarm_days_list, self._until_this_time
+        ２．デバックモードが指定された場合、entriesをdebug_entries.txtへ出力する。
+        '''
+
+        # sitemap調査用。debugモードの場合のみ。
+        if self._debug_flg:
+            _file = open('debug_entries.txt', 'a')
+            for _entry in entries:
+                _file.write(_entry['lastmod'] + ', ' + _entry['loc'] + '\n')
+            _file.close()
+
+        # クロール開始時間
+        self._crawl_start_time = datetime.now().astimezone(
+            self.settings['TIMEZONE'])
+        self._crawl_start_time_iso = self._crawl_start_time.isoformat()
+
+        # 直近の数分間の指定がある場合
+        if self._lastmod_recent_time != 0:
+            self._until_this_time = self._crawl_start_time - \
+                timedelta(minutes=self._lastmod_recent_time)
+        # 直近の数日間の指定がある場合
+        if self._term_days != 0:
+            self._tarm_days_list: list = [
+                (self._crawl_start_time - timedelta(days=i)).strftime('%y%m%d') for i in range(self._term_days)]
+        # 前回からの続きの指定がある場合
+        if self._continued:
+            self._until_this_time = parser.parse(
+                self._crawler_controller_recode[self.name][self.sitemap_urls[self._sitemap_urls_count]]['latest_lastmod'])
--- news_crawl/spiders/sankei_com_sitemap.py
+++ news_crawl/spiders/sankei_com_sitemap.py
@@ -1,15 +1,23 @@
-from datetime import datetime, timedelta
 from dateutil import parser
 import re
 from news_crawl.spiders.extensions_sitemap import ExtensionsSitemapSpider
 
-class ExtensionsSitemapFilterTyep1Spider(ExtensionsSitemapSpider):
-    '''
-    SitemapSpiderの機能を拡張したクラス。
-    Override → sitemap_filter()
-    (前提)name,allowed_domains,sitemap_urls,_domain_nameの値は当クラスを継承するクラスで設定すること
-    '''
-    name = 'extension_sitemap_filter_type1'
+
+class SankeiComSitemapSpider(ExtensionsSitemapSpider):
+    name: str = 'sankei_com_sitemap'
+    allowed_domains: list = ['sankei.com']
+    sitemap_urls: list = ['https://www.sankei.com/sitemap.xml', ]
+    _domain_name: str = 'sankei_com'        # 各種処理で使用するドメイン名の一元管理
+    spider_version: float = 1.0
+
+    # sitemap_urlsに複数のサイトマップを指定した場合、その数だけsitemap_filterが可動する。その際、どのサイトマップか判別できるように処理中のサイトマップと連動するカウント。
+    _sitemap_urls_count: int = 0
+    # crawler_controllerコレクションへ書き込むレコードのdomain以降のレイアウト雛形。※最上位のKeyのdomainはサイトの特性にかかわらず固定とするため。
+    _sitemap_contened: dict = {name: {},}
+
+    def __init__(self, *args, **kwargs):
+        super(SankeiComSitemapSpider, self).__init__(*args, **kwargs)
+        #print('=== SankeiComSitemapSpider の__init__終了')
 
     def sitemap_filter(self, entries):
         '''
@@ -19,18 +27,25 @@ class ExtensionsSitemapFilterTyep1Spider(ExtensionsSitemapSpider):
         サイトマップから取得したurlへrequestを行う前に条件で除外することができる。
         対象のurlの場合、”yield entry”で返すと親クラス側でrequestされる。
         '''
-        # sitemap_filter共通処理
+        # ExtensionsSitemapSpiderクラスを継承した場合のsitemap_filter共通処理
         self.sitemap_filter_common_prosses(entries)
 
         # 1件目のlastmod（最新の更新）とurlを取得
-        _entry: dict = next(iter(entries))
-        self._latest_lastmod = _entry['lastmod']
-        self._latest_url = _entry['loc']
+        # _entry: dict = next(iter(entries))
+        # self._latest_lastmod = _entry['lastmod']
+        # self._latest_url = _entry['loc']
+
+        _max_lstmod: str = ''
+        _max_url: str = ''
 
         for _entry in entries:
             '''
             引数に絞り込み指定がある場合、その条件を満たす場合のみ対象とする。
             '''
+            if _max_lstmod < _entry['lastmod']:
+                _max_lstmod = _entry['lastmod']
+                _max_url = _entry['loc']
+
             _crwal_flg: bool = True
             date_lastmod = parser.parse(_entry['lastmod']).astimezone(
                 self.settings['TIMEZONE'])
@@ -49,14 +64,16 @@ class ExtensionsSitemapFilterTyep1Spider(ExtensionsSitemapSpider):
                 if date_lastmod < self._until_this_time:
                     _crwal_flg = False
                 elif date_lastmod == self._until_this_time \
-                        and self._crawler_controller_recode['spider_name'][self.name]['latest_url']:
+                        and self._crawler_controller_recode[self.name][self.sitemap_urls[self._sitemap_urls_count]]['latest_url']:
                     _crwal_flg = False
 
             if _crwal_flg:
                 yield _entry
-            # else:
-            #     _pattern = re.compile('.xml$')
-            #     if _pattern.search(_entry['loc']):
-            #         #print('.')
-            #         yield _entry
 
+        self._sitemap_contened[self.name][self.sitemap_urls[self._sitemap_urls_count]] = {
+            'latest_lastmod': _max_lstmod,
+            'latest_url': _max_url,
+            'crawl_start_time': self._crawl_start_time_iso,
+        }
+
+        self._sitemap_urls_count += 1  # 次のサイトマップurl用にカウントアップ
