99174e20f4f260aa58ef1b7d5e2b7e381a10a212
pubranko <mikuras3@outlook.com>
2021-05-06 23:36:00 +0900

    読売のサイトマップ完成。ロイターのサイトマップ完成。 しかしロイターは別途新着用が必要であることが判明。

--- news_crawl/spiders/extensions_sitemap.py
+++ news_crawl/spiders/extensions_sitemap.py
@@ -3,6 +3,7 @@ from scrapy.spiders import SitemapSpider
 from datetime import datetime
 import pickle
 import sys
+import re
 from news_crawl.items import NewsCrawlItem
 from news_crawl.models.mongo_model import MongoModel
 from news_crawl.models.crawler_controller_model import CrawlerControllerModel
@@ -64,39 +65,7 @@ class ExtensionsSitemapSpider(SitemapSpider):
             TIMEZONE)
         self._crawl_start_time_iso = self._crawl_start_time.isoformat()
 
-        print('=== ExtensionsSitemapSpider の __init__終了')
-
-    def parse(self, response):
-        '''
-        取得したレスポンスよりDBへ書き込み
-        '''
-        _info = self.name + ':' + str(self.spider_version) + ' / ' \
-            + 'extensions_sitemap:' + str(self._extensions_sitemap_version)
-
-        yield NewsCrawlItem(
-            url=response.url,
-            response_time=datetime.now().astimezone(self.settings['TIMEZONE']),
-            response_headers=pickle.dumps(response.headers),
-            response_body=pickle.dumps(response.body),
-            spider_version_info=_info
-        )  # ログに不要な値を出さないようitems.pyで細工して文字列を短縮。
-
-    def closed(self, spider):
-        '''
-        spider終了時、次回クロール向けの情報をcrawler_controllerへ記録する。
-        '''
-        self.logger.info('=== Spider closed: %s', self.name)
-        crawler_controller = CrawlerControllerModel(self.mongo)
-        crawler_controller.update(
-            {'domain': self._domain_name},
-            {'domain': self._domain_name,
-             self.name: self._sitemap_next_crawl_info[self.name],
-             },
-        )
-
-        self.mongo.close()
-
-    def __argument_check(self, mongo: MongoModel, domain_name: str, crawler_controller_recode:Any, *args, **kwargs) -> None:
+    def __argument_check(self, mongo: MongoModel, domain_name: str, crawler_controller_recode: Any, *args, **kwargs) -> None:
         '''あとで
         '''
         ### 単項目チェック ###
@@ -135,8 +104,80 @@ class ExtensionsSitemapSpider(SitemapSpider):
         if 'lastmod_recent_time' in kwargs and 'sitemap_continued' in kwargs:
             sys.exit('引数エラー：lastmod_recent_timeとsitemap_continuedは同時には使えません。')
 
-    def __argument_processing(self):
-        pass
+    def sitemap_filter(self, entries):
+        '''
+        親クラスのSitemapSpiderの同名メソッドをオーバーライド。
+        entriesには、サイトマップから取得した情報(loc,lastmodなど）が辞書形式で格納されている。
+        例)entryの使い方：entry['lastmod'],entry['loc'],entry.items()
+        サイトマップから取得したurlへrequestを行う前に条件で除外することができる。
+        対象のurlの場合、”yield entry”で返すと親クラス側でrequestされる。
+        '''
+        # ExtensionsSitemapSpiderクラスを継承した場合のsitemap_filter共通処理
+        self.sitemap_filter_common_prosses(entries)
+
+        # 直近の数分間の指定がある場合
+        until_this_time: datetime = self._crawl_start_time
+        if 'lastmod_recent_time' in self.kwargs_save:
+            until_this_time = until_this_time - \
+                timedelta(minutes=int(self.kwargs_save['lastmod_recent_time']))
+
+        # urlに含まれる日付に指定がある場合
+        _url_term_days_list: list = []
+        if 'url_term_days' in self.kwargs_save:   #
+            _url_term_days_list = self.term_days_Calculation(
+                self._crawl_start_time, int(self.kwargs_save['url_term_days']), '%y%m%d')
+
+        # 前回からの続きの指定がある場合
+        _last_time: datetime = datetime.now()
+        if 'sitemap_continued' in self.kwargs_save:
+            _last_time = parser.parse(
+                self._crawler_controller_recode[self.name][self.sitemap_urls[self._sitemap_urls_count]]['latest_lastmod'])
+
+        # 処理中のサイトマップ内で、最大のlastmodとurlを記録するエリア
+        _max_lstmod: str = ''
+        _max_url: str = ''
+
+        for _entry in entries:
+            '''
+            引数に絞り込み指定がある場合、その条件を満たす場合のみ対象とする。
+            '''
+            if _max_lstmod < _entry['lastmod']:
+                _max_lstmod = _entry['lastmod']
+                _max_url = _entry['loc']
+
+            _crwal_flg: bool = True
+            _date_lastmod = parser.parse(_entry['lastmod']).astimezone(
+                self.settings['TIMEZONE'])
+
+            if 'url_pattern' in self.kwargs_save:   # url絞り込み指定あり
+                pattern = re.compile(self.kwargs_save['url_pattern'])
+                if pattern.search(_entry['loc']) == None:
+                    _crwal_flg = False
+            if 'url_term_days' in self.kwargs_save:                       # 期間指定あり
+                _pattern = re.compile('|'.join(_url_term_days_list))
+                if _pattern.search(_entry['loc']) == None:
+                    _crwal_flg = False
+            if 'lastmod_recent_time' in self.kwargs_save:             # lastmod絞り込み指定あり
+                if _date_lastmod < until_this_time:
+                    _crwal_flg = False
+            if 'sitemap_continued' in self.kwargs_save:
+                if _date_lastmod < _last_time:
+                    _crwal_flg = False
+                elif _date_lastmod == _last_time \
+                        and self._crawler_controller_recode[self.name][self.sitemap_urls[self._sitemap_urls_count]]['latest_url']:
+                    _crwal_flg = False
+
+            if _crwal_flg:
+                yield _entry
+
+        # サイトマップごとの最大更新時間を記録(crawler_controllerコレクションへ保存する内容)
+        self._sitemap_next_crawl_info[self.name][self.sitemap_urls[self._sitemap_urls_count]] = {
+            'latest_lastmod': _max_lstmod,
+            'latest_url': _max_url,
+            'crawl_start_time': self._crawl_start_time_iso,
+        }
+
+        self._sitemap_urls_count += 1  # 次のサイトマップurl用にカウントアップ
 
     def sitemap_filter_common_prosses(self, entries):
         ''' 独自に拡張。
@@ -144,9 +185,7 @@ class ExtensionsSitemapSpider(SitemapSpider):
           => self._crawl_start_time, self._crawl_start_time_iso, self._until_this_time, self._tarm_days_list*, self._until_this_time
         ２．デバックモードが指定された場合、entriesをdebug_entries.txtへ出力する。
         '''
-
-        # sitemap調査用。debugモードの場合のみ。
-        if 'debug' in self.kwargs_save:
+        if 'debug' in self.kwargs_save:         # sitemap調査用。debugモードの場合のみ。
             _ = open('debug_entries.txt', 'a')
             for _entry in entries:
                 _.write(_entry['lastmod'] + ', ' + _entry['loc'] + '\n')
@@ -158,3 +197,33 @@ class ExtensionsSitemapSpider(SitemapSpider):
         日付の形式(date_pattern)には、datetime.strftimeのパターンを指定する。
         '''
         return [(crawl_start_time - timedelta(days=i)).strftime(date_pattern) for i in range(term_days)]
+
+    def parse(self, response):
+        '''
+        取得したレスポンスよりDBへ書き込み
+        '''
+        _info = self.name + ':' + str(self.spider_version) + ' / ' \
+            + 'extensions_sitemap:' + str(self._extensions_sitemap_version)
+
+        yield NewsCrawlItem(
+            url=response.url,
+            response_time=datetime.now().astimezone(self.settings['TIMEZONE']),
+            response_headers=pickle.dumps(response.headers),
+            response_body=pickle.dumps(response.body),
+            spider_version_info=_info
+        )  # ログに不要な値を出さないようitems.pyで細工して文字列を短縮。
+
+    def closed(self, spider):
+        '''
+        spider終了時、次回クロール向けの情報をcrawler_controllerへ記録する。
+        '''
+        self.logger.info('=== Spider closed: %s', self.name)
+        crawler_controller = CrawlerControllerModel(self.mongo)
+        crawler_controller.update(
+            {'domain': self._domain_name},
+            {'domain': self._domain_name,
+             self.name: self._sitemap_next_crawl_info[self.name],
+             },
+        )
+
+        self.mongo.close()
---
+++ news_crawl/spiders/jp_reuters_com.py
@@ -0,0 +1,39 @@
+import sys
+from news_crawl.spiders.extensions_sitemap import ExtensionsSitemapSpider
+from datetime import datetime, timedelta
+
+
+class JpReutersComSpider(ExtensionsSitemapSpider):
+    name = 'jp_reuters_com'
+    allowed_domains = ['jp.reuters.com']
+    sitemap_urls: list = []
+    _domain_name: str = 'jp_reuters_com'        # 各種処理で使用するドメイン名の一元管理
+    spider_version: float = 1.0
+
+    # sitemap_urlsに複数のサイトマップを指定した場合、その数だけsitemap_filterが可動する。その際、どのサイトマップか判別できるように処理中のサイトマップと連動するカウント。
+    _sitemap_urls_count: int = 0
+    # crawler_controllerコレクションへ書き込むレコードのdomain以降のレイアウト雛形。※最上位のKeyのdomainはサイトの特性にかかわらず固定とするため。
+    _sitemap_next_crawl_info: dict = {name: {}, }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 単項目チェック（追加）
+        if not 'sitemap_term_days' in kwargs:
+            sys.exit('引数エラー：当スパイダー(' + self.name +
+                     ')の場合、sitemap_term_daysは必須です。')
+
+        # 以下のようなurlを生成する。
+        #     https://jp.reuters.com/sitemap_20210505-20210506.xml,
+        #     https://jp.reuters.com/sitemap_20210504-20210505.xml
+
+        _sitemap_term_days_list_start = self.term_days_Calculation(
+            self._crawl_start_time - timedelta(days=1), int(self.kwargs_save['sitemap_term_days']), '%Y%m%d')
+        _sitemap_term_days_list_end = self.term_days_Calculation(
+            self._crawl_start_time, int(self.kwargs_save['sitemap_term_days']), '%Y%m%d')
+
+        self.sitemap_urls = [
+            'https://jp.reuters.com/sitemap_%s-%s.xml' % (s, e) for s, e in zip(_sitemap_term_days_list_start, _sitemap_term_days_list_end)]
+        # 'https://www.yomiuri.co.jp/sitemap-pt-post-%s.xml' % (i) for i in _sitemap_term_days_list]
+
+        self.logger.info('sitemap_urls 生成完了: %s', self.sitemap_urls)
