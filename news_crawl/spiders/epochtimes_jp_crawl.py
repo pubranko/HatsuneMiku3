369f59a3c38b5a4fc464d64117c90988e0063cfc
pubranko <mikuras3@outlook.com>
2021-05-29 12:56:32 +0900

    epochtimes_jp_crawlのリファクタリング、バグ修正完了。

--- news_crawl/spiders/epochtimes_jp_crawl.py
+++ news_crawl/spiders/epochtimes_jp_crawl.py
@@ -1,20 +1,12 @@
 from typing import Pattern
 from bs4.element import ResultSet
 from news_crawl.spiders.extensions_crawl import ExtensionsCrawlSpider
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import Rule
 from scrapy.http.response.html import HtmlResponse
-from scrapy_selenium import SeleniumRequest
-import urllib.parse
-import re,sys
+import re
+import sys
 import scrapy
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import concurrent.futures
 from bs4 import BeautifulSoup as bs4
+import copy
 
 
 class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
@@ -34,6 +26,8 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
     _crawl_urls_count: int = 0
     # crawler_controllerコレクションへ書き込むレコードのdomain以降のレイアウト雛形。※最上位のKeyのdomainはサイトの特性にかかわらず固定とするため。
     _crawl_next_info: dict = {}
+    # 各カテゴリー別に前回取得済みのurls情報を保存
+    _last_time_urls: dict = {}
 
     def __init__(self, *args, **kwargs):
         ''' (拡張メソッド)
@@ -46,6 +40,7 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
             sys.exit('引数エラー：当スパイダー(' + self.name +
                      ')の場合、category_urlsは必須です。')
 
+        # 前回のクロール情報を次回向けへ引き継ぎ
         self._crawl_next_info = self._crawler_controller_recode
 
     def start_requests(self):
@@ -56,7 +51,8 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
         pages: dict = self.pages_setting(1, 5)
         start_page: int = pages['start_page']
 
-        category_urls: list = eval(self.kwargs_save['category_urls'])   #[100, 108, 170, 169, 101]
+        # [100, 108, 170, 169, 101]
+        category_urls: list = eval(self.kwargs_save['category_urls'])
         urls: list = ['https://www.epochtimes.jp/category/%s' %
                       str(i) + '/' + str(start_page) + '.html' for i in category_urls]
 
@@ -69,9 +65,7 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
         取得したレスポンスよりDBへ書き込み
         '''
         # ループ条件
-        # 1.現在のページ数は、5ページまで（仮）
-        # 2.前回の1ページ目の記事リンク（10件）まで全て遡りきったら、前回以降に追加された記事は取得完了と考えられるため終了。
-
+        # 無制限にクロールしないよう、標準設定を3ページ目までとする。
         pages: dict = self.pages_setting(1, 3)
         start_page: int = pages['start_page']
         end_page: int = pages['end_page']
@@ -92,11 +86,13 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
             r'https://www.epochtimes.jp/category/[0-9]{3}/', response.url)[0]
         url_footer: str = re.findall(r'\.html', response.url)[0]
 
-        # 前回からの続きの指定がある場合、前回のまでのクロール情報を取得する。
-        last_time: dict = {}
-        last_time_urls :list = []
-        if 'continued' in self.kwargs_save:
-            last_time_urls = self._crawl_next_info[self.name]['urls']
+        # 各カテゴリーの最初のページ、and、前回からの続きの指定がある場合
+        # 前回のまでのクロール情報からurlsを各カテゴリー別に保存する。
+        if response.url in self.start_urls and 'continued' in self.kwargs_save:
+            self._last_time_urls[url_header] = copy.deepcopy(
+                self._crawl_next_info[self.name][url_header]['urls'])
+
+        end_flg = False
 
         # 現在のページ内の記事のリンクをリストへ保存
         # １件目のアンカーと２件目以降のアンカーを取得。（１ページ目だけ１件目が<table>内に無い）
@@ -106,37 +102,38 @@ class EpochtimesJpCrawlSpider(ExtensionsCrawlSpider):
         for anchor in anchors:
             url: str = 'https://www.epochtimes.jp/p' + anchor.get('href')
             urls_list.append(url)
-            # 前回取得したurlが確認できたら確認済み（削除）にする。
-            if url in last_time_urls:
-                last_time_urls.remove(url)
+            # 前回からの続きの指定がある場合
+            if 'continued' in self.kwargs_save:
+                # 前回取得したurlが確認できたら確認済み（削除）にする。
+                if url in self._last_time_urls[url_header]:
+                    self._last_time_urls[url_header].remove(url)
+                # 前回の１ページ目のurlが全て確認できたら前回以降に追加された記事は全て取得完了と考えられるため終了する。
+                if len(self._last_time_urls[url_header]) == 0:
+                    self.logger.info(
+                        '=== parse_start_response 前回の続きまで再取得完了 (%s)', response.url)
+                    end_flg = True
+                    break
 
         self.logger.info(
             '=== parse_start_response リンク件数 = %s', len(urls_list))
 
         self.common_prosses(response.url, urls_list)
 
-        # 前回からの続きの指定がある場合、前回の１ページ目のurlが全て確認できたら前回以降に追加された記事は全て取得完了と考えられるため終了する。
-        end_flg = False
-        if 'continued' in self.kwargs_save:
-            if len(last_time) == 0:
-                self.logger.info(
-                    '=== parse_start_response 前回の続きまで再取得完了 (%s)', response.url)
-                end_flg = True
-
         # 終了ページを超えたら終了する。
         if now_page + 1 > end_page:
             end_flg = True
 
-        if end_flg == True:
-            # 次回に向け、現在クロール中のカテゴリーの情報を更新する。
-            # ・1ページ目の30件をcrawler_controllerへ保存
-            # ・Keyとなるurlには、http〜各カテゴリー(url_header)までの一部とする。
+        # 各カテゴリーの最初のページの場合、次回に向け現在クロール中のカテゴリーの情報を更新する。
+        # ・1ページ目の10件をcrawler_controllerへ保存
+        # ・Keyとなるurlには、http〜各カテゴリー(url_header)までの一部とする。
+        if response.url in self.start_urls:
             self._crawl_next_info[self.name][url_header] = {
-                'urls': urls_list[0:30],
+                'urls': urls_list[0:10],
                 'crawl_start_time': self._crawl_start_time_iso,
             }
-        else:
-            # 次のページを読み込む
+
+        # 続きがある場合、次のページを読み込む
+        if end_flg == False:
             url_next: str = url_header + str(now_page + 1) + url_footer
             self.logger.info(
                 '=== parse_start_response 次のページのURL = %s', url_next)
