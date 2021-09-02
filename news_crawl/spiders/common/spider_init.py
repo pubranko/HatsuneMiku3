bba73a59c4ed3dc58a9bde3fd0163c05e53bb5cd
pubranko <mikuras3@outlook.com>
2021-09-02 23:29:58 +0900

    sitemap抽出条件の部品化完了。controller内の文字列日付のISODate化完了

--- news_crawl/spiders/common/spider_init.py
+++ news_crawl/spiders/common/spider_init.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dateutil import parser
 from logging import Logger
 from scrapy.exceptions import CloseSpider
 from models.mongo_model import MongoModel
@@ -9,6 +10,7 @@ from news_crawl.spiders.common.argument_check import argument_check
 from news_crawl.spiders.common.start_request_debug_file_init import start_request_debug_file_init
 from news_crawl.spiders.common.crawling_domain_duplicate_check import CrawlingDomainDuplicatePrevention
 from common_lib.resource_check import resource_check
+
 
 def spider_init(spider, *args, **kwargs):
     '''spider共通の初期処理'''
@@ -38,7 +40,7 @@ def spider_init(spider, *args, **kwargs):
     if not duplicate_check:
         raise CloseSpider('同一ドメインへの多重クローリングとなるため中止')
 
-    resource:dict = resource_check()
+    resource: dict = resource_check()
     # CPUチェック
     if float(str(resource['cpu_percent'])) > 90:
         logger.warning('=== ＣＰＵ使用率が90%を超えています。')
@@ -53,12 +55,13 @@ def spider_init(spider, *args, **kwargs):
     elif float(str(resource['swap_memory_percent'])) > 95:
         raise CloseSpider('=== スワップメモリー使用率が95%を超えたためスパイダーを停止します。')
 
-    # クロール開始時間
+    # クロール開始時間をスパイダーのクラス変数に保存
     if 'crawling_start_time' in spider.kwargs_save:
-        crawling_start_time = spider.kwargs_save['crawling_start_time']
+        crawling_start_time = parser.parse(
+            spider.kwargs_save['crawling_start_time'])
     else:
-        crawling_start_time = datetime.now().astimezone(
-            TIMEZONE)
+        crawling_start_time = datetime.now().astimezone(TIMEZONE)
+
     spider._crawling_start_time = crawling_start_time
 
     logger.info(
