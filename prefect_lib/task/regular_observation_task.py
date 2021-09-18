8f8cfa8236059e23018f33cecd73dc78cb682343
pubranko <mikuras3@outlook.com>
2021-09-18 22:54:09 +0900

    debug構成実装。一部リファクタリング。

--- prefect_lib/task/regular_observation_task.py
+++ prefect_lib/task/regular_observation_task.py
@@ -1,9 +1,6 @@
 # pylint: disable=E1101
 import os
 import sys
-from typing import Any
-import logging
-from datetime import datetime, timedelta
 from logging import Logger
 import threading
 path = os.getcwd()
@@ -15,7 +12,6 @@ from models.crawler_response_model import CrawlerResponseModel
 from models.scraped_from_response_model import ScrapedFromResponse
 from models.news_clip_master_model import NewsClipMaster
 from models.controller_model import ControllerModel
-from common_lib.timezone_recovery import timezone_recovery
 from common_lib.directory_search import directory_search
 
 
@@ -37,39 +33,42 @@ class RegularObservationTask(ExtensionsTask):
         kwargs['news_clip_master'] = NewsClipMaster(mongo)
         kwargs['controller'] = ControllerModel(self.mongo)
 
-        # 前回のstart_timeから、前回処理以降に追加された
-        # controller = ControllerModel(mongo)
-
         kwargs['domain'] = None
         kwargs['crawling_start_time_from'] = self.start_time
-        kwargs['crawling_start_time_to'] = None
+        kwargs['crawling_start_time_to'] = self.start_time
         kwargs['scrapying_start_time_from'] = self.start_time
-        kwargs['scrapying_start_time_to'] = None
+        kwargs['scrapying_start_time_to'] = self.start_time
         kwargs['urls'] = []
         kwargs['scraped_save_start_time_from'] = self.start_time
-        kwargs['scraped_save_start_time_to'] = None
+        kwargs['scraped_save_start_time_to'] = self.start_time
 
         spiders_info = directory_search()
         controller: ControllerModel = kwargs['controller']
         spider_name_set: set = controller.regular_observation_spider_name_set_get()
 
-        crawling_target_spiders:list = []
-        crawling_subject_spiders:list = []
+        # 定点観測の対象・対象外spiderを振り分け
+        crawling_target_spiders: list = []
+        crawling_target_spiders_name: list = []
+        crawling_subject_spiders_name: list = []
         for spider_info in spiders_info:
             if spider_info['spider_name'] in spider_name_set:
                 crawling_target_spiders.append(spider_info)
+                crawling_target_spiders_name.append(spider_info['spider_name'])
             else:
-                crawling_subject_spiders.append(spider_info)
+                crawling_subject_spiders_name.append(
+                    spider_info['spider_name'])
 
         kwargs['spiders_info'] = crawling_target_spiders
 
-        logger.info('=== 定点観測対象外スパイダー : ' + str(crawling_subject_spiders))
-        logger.info('=== 定点観測 run kwargs : ' + str(kwargs))
+        logger.info('=== 定点観測対象スパイダー : ' +
+                    str(crawling_target_spiders_name) + '\n')
+        logger.info('=== 定点観測対象外スパイダー : ' +
+                    str(crawling_subject_spiders_name) + '\n')
+        logger.info('=== 定点観測 run kwargs : ' + str(kwargs) + '\n')
 
+        thread = threading.Thread(
+            target=scrapy_crawling_run.continued_run(kwargs))
 
-        # thread = threading.Thread(target=scrapy_crawling_continue_run.exec(
-        #     self.start_time, kwargs['controller']))
-        thread = threading.Thread(target=scrapy_crawling_run.continued_run(kwargs))
         # マルチプロセスで動いているScrapyの終了を待ってから後続の処理を実行する。
         thread.start()
         thread.join()
