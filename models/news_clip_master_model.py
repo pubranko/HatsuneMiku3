b4288968efa82e6e1f191afc0a23958033d385ee
pubranko <mikuras3@outlook.com>
2021-10-09 15:53:20 +0900

    mongoDBのmodelの共通クラスを作成→各modelは継承して使うよう修正

--- models/news_clip_master_model.py
+++ models/news_clip_master_model.py
@@ -1,20 +1,28 @@
 from models.mongo_model import MongoModel
+from models.mongo_common_model import MongoCommonModel
 
 
-class NewsClipMaster(object):
+class NewsClipMaster(MongoCommonModel):
     '''
     news_clip_masterコレクション用モデル
     '''
     mongo: MongoModel
+    collection_name: str = 'news_clip_master'
 
-    def __init__(self, mongo: MongoModel):
-        self.mongo = mongo
+# class NewsClipMaster(object):
+#     '''
+#     news_clip_masterコレクション用モデル
+#     '''
+#     mongo: MongoModel
 
-    def insert_one(self, item:dict):
-        self.mongo.crawler_db['news_clip_master'].insert_one(item)
+#     def __init__(self, mongo: MongoModel):
+#         self.mongo = mongo
 
-    def find_one(self, projection=None,filter=None):
-        return self.mongo.crawler_db['news_clip_master'].find_one(projection=projection,filter=filter)
+#     def insert_one(self, item:dict):
+#         self.mongo.mongo_db['news_clip_master'].insert_one(item)
 
-    def find(self, projection=None,filter=None, sort=None):
-        return self.mongo.crawler_db['news_clip_master'].find(projection=projection,filter=filter,sort=sort)
+#     def find_one(self, projection=None,filter=None):
+#         return self.mongo.mongo_db['news_clip_master'].find_one(projection=projection,filter=filter)
+
+#     def find(self, projection=None,filter=None, sort=None):
+#         return self.mongo.mongo_db['news_clip_master'].find(projection=projection,filter=filter,sort=sort)
--- models/scraped_from_response_model.py
+++ models/scraped_from_response_model.py
@@ -1,23 +1,31 @@
 from models.mongo_model import MongoModel
+from models.mongo_common_model import MongoCommonModel
 
 
-class ScrapedFromResponse(object):
+class ScrapedFromResponse(MongoCommonModel):
     '''
     scraped_from_responseコレクション用モデル
     '''
     mongo: MongoModel
+    collection_name: str = 'scraped_from_response'
 
-    def __init__(self, mongo: MongoModel):
-        self.mongo = mongo
+# class ScrapedFromResponse(object):
+#     '''
+#     scraped_from_responseコレクション用モデル
+#     '''
+#     mongo: MongoModel
 
-    def insert_one(self, item:dict):
-        self.mongo.crawler_db['scraped_from_response'].insert_one(item)
+#     def __init__(self, mongo: MongoModel):
+#         self.mongo = mongo
 
-    def insert(self, items:list):
-        self.mongo.crawler_db['scraped_from_response'].insert(items)
+#     def insert_one(self, item:dict):
+#         self.mongo.mongo_db['scraped_from_response'].insert_one(item)
 
-    def find_one(self, projection=None,filter=None):
-        return self.mongo.crawler_db['scraped_from_response'].find_one(projection=projection,filter=filter)
+#     def insert(self, items:list):
+#         self.mongo.mongo_db['scraped_from_response'].insert(items)
 
-    def find(self, projection=None,filter=None, sort=None):
-        return self.mongo.crawler_db['scraped_from_response'].find(projection=projection,filter=filter,sort=sort)
+#     def find_one(self, projection=None,filter=None):
+#         return self.mongo.mongo_db['scraped_from_response'].find_one(projection=projection,filter=filter)
+
+#     def find(self, projection=None,filter=None, sort=None):
+#         return self.mongo.mongo_db['scraped_from_response'].find(projection=projection,filter=filter,sort=sort)
