c4b348936e11b62f0b506d00b380ddc823aa9175
pubranko <mikuras3@outlook.com>
2021-10-09 13:52:01 +0900

    masterとsolrの同期チェック実装完了

--- common_lib/timezone_recovery.py
+++ common_lib/timezone_recovery.py
@@ -2,13 +2,19 @@ from datetime import datetime
 from dateutil import tz
 from prefect_lib.settings import TIMEZONE
 
-def timezone_recovery(dt:datetime) -> datetime:
+def timezone_recovery(dt:datetime,timezone:str='') -> datetime:
     '''
     MongoDBから取得したISODate型はタイムゾーンが抜け落ちている。
     それの補正とタイムゾーンの付与を行ったdatetime型を返す。
     MongoDBから取得したISODate型の項目を使う場合、基本的に当関数を通じて使用すること。
     '''
-    UTC = tz.gettz("UTC")
-    dt = dt.replace(tzinfo=UTC)
-    dt = dt.astimezone(TIMEZONE)
+    if timezone == '':
+        UTC = tz.gettz("UTC")
+        dt = dt.replace(tzinfo=UTC)
+        dt = dt.astimezone(TIMEZONE)
+    else:
+        UTC = tz.gettz("UTC")
+        dt = dt.replace(tzinfo=UTC)
+        _ = tz.gettz(timezone)
+        dt = dt.astimezone(_)
     return dt
--- prefect_lib/test/solr_recovery_del.py
+++ prefect_lib/test/solr_recovery_del.py
@@ -19,16 +19,14 @@ def solr_del():
 
     #publish_date :[2019-01-01T00:00:00Z TO 2019-01-31T23:59:59Z]
     #'2021-08-21T09:06:18+09:00 TO 2021-08-21T09:06:19+09:00'
-    solr.delete_query(
-        #'response_time:[2021-08-21T00:06:18Z TO 2021-08-21T00:06:19Z]',
-        'response_time:[2021-08-20T00:06:18Z TO *]',
-    )
+    # solr.delete_query(
+    #     #'response_time:[2021-08-21T00:06:18Z TO 2021-08-21T00:06:19Z]',
+    #     'response_time:[2021-08-20T00:06:18Z TO *]',
+    # )
 
-    #solr.delete(id=["e234a380-e9c9-44f0-8b50-d0181c8a057e"])
-    # solr.delete(id=[
-    #     "94dcbcce-fe9a-4cab-b134-450e1bb04526",
-    #     "2f312294-1ea4-45b9-a925-dcc45cdeed5f",
-    # ])
+    solr.delete_id(delete_id_list=[
+        "1baf825c-d8cb-4eab-aa54-f1b8891e8d3d",
+    ])
 
     solr.commit()
 
