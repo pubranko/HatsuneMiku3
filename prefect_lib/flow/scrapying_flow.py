acd496f27992b55925880fa6cc143e20a5aea897
pubranko <mikuras3@outlook.com>
2021-10-09 14:49:05 +0900

    リファクタリング：prefectのFlowにあるstatus_changeの共通化

--- prefect_lib/flow/scrapying_flow.py
+++ prefect_lib/flow/scrapying_flow.py
@@ -1,21 +1,17 @@
 import os
 import sys
-from datetime import datetime
 import logging
-path = os.getcwd()
-sys.path.append(path)
-import prefect
+from datetime import datetime
 from prefect import Flow, task, Parameter
 from prefect.core.parameter import DateTimeParameter
 from prefect.tasks.control_flow.conditional import ifelse
 from prefect.engine import signals
-# ステータス一覧： Running,Success,Failed,Cancelled,TimedOut,TriggerFailed,ValidationFailed,Skipped,Mapped,Cached,Looped,Finished,Cancelling,Retrying,Resume,Queued,Submitted,ClientFailed,Paused,Scheduled,Pending
-from prefect.engine.state import Running, Success, Failed
+from prefect.utilities.context import Context
+path = os.getcwd()
+sys.path.append(path)
 from prefect_lib.task.scrapying_task import ScrapyingTask
 from prefect_lib.settings import TIMEZONE
-from prefect.utilities.context import Context
-from common_lib.mail_send import mail_send
-
+from prefect_lib.common_module.flow_status_change import flow_status_change
 
 start_time = datetime.now().astimezone(
     TIMEZONE)
@@ -24,36 +20,16 @@ log_file_path = os.path.join(
 logging.basicConfig(level=logging.DEBUG, filemode="w+", filename=log_file_path,
                     format='%(asctime)s %(levelname)s [%(name)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-
-def status_change(obj: Flow, old_state, new_state):
-    '''フローのステータスが変更される際に必要な処理を行う'''
-    if isinstance(new_state, Running):
-        pass  # フロー開始時
-    elif isinstance(new_state, Success):
-        pass  # 正常終了時の処理
-    elif isinstance(new_state, Failed):
-        # logファイルをメールで送信
-        with open(log_file_path) as f:
-            log_file = f.read()
-        # mail_send('【prefectフローでエラー発生】' +
-        #           crawling_start_time.isoformat(), log_file,)
-
-    if not isinstance(new_state, Running):
-        pass  # 成否に関係なく終わったときに動く処理
-
-
 with Flow(
     name='Scrapying flow',
-    state_handlers=[status_change],
+    state_handlers=[flow_status_change],
 ) as flow:
-
     domain = Parameter('domain', required=False)()
     crawling_start_time_from = DateTimeParameter(
         'crawling_start_time_from', required=False,)
     crawling_start_time_to = DateTimeParameter(
         'crawling_start_time_to', required=False)
     urls = Parameter('urls', required=False)()
-
     task = ScrapyingTask(
         log_file_path=log_file_path, start_time=start_time)
     result = task(domain=domain,
@@ -68,4 +44,3 @@ flow.run(parameters=dict(
     crawling_start_time_to=datetime(2021, 9, 25, 11, 8, 28, 286000).astimezone(TIMEZONE),
     #urls=['https://www.sankei.com/article/20210829-2QFVABFPMVIBNHSINK6TBYWEXE/?outputType=theme_tokyo2020',]
 ))
-#2021-08-29T13:33:48.503Z
--- prefect_lib/flow/stop_controller_update_flow.py
+++ prefect_lib/flow/stop_controller_update_flow.py
@@ -2,20 +2,15 @@ import os
 import sys
 from datetime import datetime
 import logging
-path = os.getcwd()
-sys.path.append(path)
-import prefect
 from prefect import Flow, task, Parameter
-from prefect.core.parameter import DateTimeParameter
 from prefect.tasks.control_flow.conditional import ifelse
 from prefect.engine import signals
-# ステータス一覧： Running,Success,Failed,Cancelled,TimedOut,TriggerFailed,ValidationFailed,Skipped,Mapped,Cached,Looped,Finished,Cancelling,Retrying,Resume,Queued,Submitted,ClientFailed,Paused,Scheduled,Pending
-from prefect.engine.state import Running, Success, Failed
+from prefect.utilities.context import Context
+path = os.getcwd()
+sys.path.append(path)
 from prefect_lib.task.stop_controller_update_task import StopControllerUpdateTask
 from prefect_lib.settings import TIMEZONE
-from prefect.utilities.context import Context
-from common_lib.mail_send import mail_send
-
+from prefect_lib.common_module.flow_status_change import flow_status_change
 
 start_time = datetime.now().astimezone(
     TIMEZONE)
@@ -24,33 +19,13 @@ log_file_path = os.path.join(
 logging.basicConfig(level=logging.DEBUG, filemode="w+", filename=log_file_path,
                     format='%(asctime)s %(levelname)s [%(name)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-
-def status_change(obj: Flow, old_state, new_state):
-    '''フローのステータスが変更される際に必要な処理を行う'''
-    if isinstance(new_state, Running):
-        pass  # フロー開始時
-    elif isinstance(new_state, Success):
-        pass  # 正常終了時の処理
-    elif isinstance(new_state, Failed):
-        # logファイルをメールで送信
-        with open(log_file_path) as f:
-            log_file = f.read()
-        # mail_send('【prefectフローでエラー発生】' +
-        #           crawling_start_time.isoformat(), log_file,)
-
-    if not isinstance(new_state, Running):
-        pass  # 成否に関係なく終わったときに動く処理
-
-
 with Flow(
     name='Stop Controller Update Flow',
-    state_handlers=[status_change],
+    state_handlers=[flow_status_change],
 ) as flow:
-
     domain = Parameter('domain', required=True)()   #登録・削除したいドメインを指定
     in_out = Parameter('in_out', required=True)()   #in:登録、out：削除
     destination = Parameter('destination', required=True)()   #crawlingとscrapyingの選択
-
     task = StopControllerUpdateTask(
         log_file_path=log_file_path, start_time=start_time)
     result = task(domain=domain, in_out=in_out,
