5c99fe9002d12148f2ee737d36539083ec356da8
pubranko <mikuras3@outlook.com>
2021-10-09 15:53:20 +0900

    mongoDBのmodelの共通クラスを作成→各modelは継承して使うよう修正

--- models/asynchronous_report_model.py
+++ models/asynchronous_report_model.py
@@ -1,20 +1,28 @@
 from models.mongo_model import MongoModel
+from models.mongo_common_model import MongoCommonModel
 
 
-class AsynchronousReportModel(object):
+class AsynchronousReportModel(MongoCommonModel):
     '''
     非同期レポートコレクション用モデル
     '''
     mongo: MongoModel
+    collection_name: str = 'asynchronous_report'
 
-    def __init__(self, mongo: MongoModel):
-        self.mongo = mongo
+# class AsynchronousReportModel(object):
+#     '''
+#     非同期レポートコレクション用モデル
+#     '''
+#     mongo: MongoModel
 
-    def insert_one(self, item: dict):
-        self.mongo.crawler_db['asynchronous_report'].insert_one(item)
+#     def __init__(self, mongo: MongoModel):
+#         self.mongo = mongo
 
-    def find_one(self, filter=None, projection=None):
-        return self.mongo.crawler_db['asynchronous_report'].find_one(projection=projection, filter=filter)
+#     def insert_one(self, item: dict):
+#         self.mongo.mongo_db['asynchronous_report'].insert_one(item)
 
-    def find(self, filter=None, projection=None, sort=None):
-        return self.mongo.crawler_db['asynchronous_report'].find(projection=projection, filter=filter, sort=sort)
+#     def find_one(self, filter=None, projection=None):
+#         return self.mongo.mongo_db['asynchronous_report'].find_one(projection=projection, filter=filter)
+
+#     def find(self, filter=None, projection=None, sort=None):
+#         return self.mongo.mongo_db['asynchronous_report'].find(projection=projection, filter=filter, sort=sort)
--- models/crawler_logs_model.py
+++ models/crawler_logs_model.py
@@ -1,28 +1,36 @@
 from models.mongo_model import MongoModel
+from models.mongo_common_model import MongoCommonModel
 
 
-class CrawlerLogsModel(object):
+class CrawlerLogsModel(MongoCommonModel):
     '''
     crawler_logsコレクション用モデル
     '''
     mongo: MongoModel
+    collection_name: str = 'crawler_logs'
 
-    def __init__(self, mongo: MongoModel):
-        self.mongo = mongo
+# class CrawlerLogsModel(object):
+#     '''
+#     crawler_logsコレクション用モデル
+#     '''
+#     mongo: MongoModel
 
-    def insert_one(self, item):
-        self.mongo.crawler_db['crawler_logs'].insert_one(item)
+#     def __init__(self, mongo: MongoModel):
+#         self.mongo = mongo
 
-    def find_one(self, projection=None, filter=None):
-        return self.mongo.crawler_db['crawler_logs'].find_one(projection=projection, filter=filter)
+#     def insert_one(self, item):
+#         self.mongo.mongo_db['crawler_logs'].insert_one(item)
 
-    def find(self, projection=None, filter=None, sort=None):
-        return self.mongo.crawler_db['crawler_logs'].find(projection=projection, filter=filter, sort=sort)
+#     def find_one(self, projection=None, filter=None):
+#         return self.mongo.mongo_db['crawler_logs'].find_one(projection=projection, filter=filter)
 
-    def aggregate(self, aggregate_key: str):
-        '''渡された集計keyによる集計結果を返す。'''
-        pipeline = [
-            {'$unwind': '$' + aggregate_key},
-            {'$group': {'_id': '$' + aggregate_key, 'count': {'$sum': 1}}},
-        ]
-        return self.mongo.crawler_db['crawler_logs'].aggregate(pipeline=pipeline)
+#     def find(self, projection=None, filter=None, sort=None):
+#         return self.mongo.mongo_db['crawler_logs'].find(projection=projection, filter=filter, sort=sort)
+
+#     def aggregate(self, aggregate_key: str):
+#         '''渡された集計keyによる集計結果を返す。'''
+#         pipeline = [
+#             {'$unwind': '$' + aggregate_key},
+#             {'$group': {'_id': '$' + aggregate_key, 'count': {'$sum': 1}}},
+#         ]
+#         return self.mongo.mongo_db['crawler_logs'].aggregate(pipeline=pipeline)
