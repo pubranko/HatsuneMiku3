7dab8a8f0c4eebb315b687f142e4ef61d15a1a5b
pubranko <mikuras3@outlook.com>
2021-10-11 08:39:13 +0900

    crawler_logsのsource_urlの格納構造リファクタリング

--- prefect_lib/run/crawl_urls_sync_check_run.py
+++ prefect_lib/run/crawl_urls_sync_check_run.py
@@ -72,9 +72,16 @@ def check(kwargs: dict):
             # domain別の集計エリアを初期設定
             response_async_domain_aggregate[log_record['domain']] = 0
 
+            # crawl_urls_listからをクロール対象となったurlを抽出
+            loc_crawl_urls: list = []
+            for temp in log_record['crawl_urls_list']:
+                loc_crawl_urls = [item['loc'] for item in temp['items']]
+
             # スパイダーレポートよりクロール対象となったurlを順に読み込み、crawler_responseに登録されていることを確認する。
-            for crawl_urls_list in log_record['crawl_urls_list']:
-                conditions.append({'url': crawl_urls_list['loc']})
+            #for crawl_urls_list in log_record['crawl_urls_list']:
+            for crawl_url in loc_crawl_urls:
+                #conditions.append({'url': crawl_urls_list['loc']})
+                conditions.append({'url': crawl_url})
                 master_filter: Any = {'$and': conditions}
                 response_records: Cursor = crawler_response.find(
                     filter=master_filter,
@@ -83,7 +90,8 @@ def check(kwargs: dict):
 
                 # crawler_response側に存在しないクロール対象urlがある場合
                 if response_records.count() == 0:
-                    response_async_list.append(crawl_urls_list['loc'])
+                    #response_async_list.append(crawl_urls_list['loc'])
+                    response_async_list.append(crawl_url)
                     # 非同期ドメイン集計カウントアップ
                     response_async_domain_aggregate[log_record['domain']] += 1
 
@@ -159,7 +167,7 @@ def check(kwargs: dict):
                 # レスポンスにあるのにマスターへの登録処理が行われていない。
                 _ = {
                     'url': master_record['url'],
-                    #'response_time': timezone_recovery(master_record['response_time']),
+                    # 'response_time': timezone_recovery(master_record['response_time']),
                     'response_time': master_record['response_time'],
                     'domain': master_record['domain'],
                 }
@@ -190,27 +198,28 @@ def check(kwargs: dict):
         solr_async_domain_aggregate: dict = {}
         solr_filter: Any = ''
 
-
         UTC = tz.gettz("UTC")
 
         # news_clip_masterで同期しているリストを順に読み込み、solr_news_clipに登録されているか確認する。
         for master_sync in master_sync_list:
-            solr_query:list = [
-                'url:' ,solr_news_clip.escape_convert(master_sync['url']),
+            solr_query: list = [
+                'url:', solr_news_clip.escape_convert(master_sync['url']),
                 ' and ',
-                'response_time:',solr_news_clip.escape_convert(master_sync['response_time'].isoformat() + 'Z'),
+                'response_time:', solr_news_clip.escape_convert(
+                    master_sync['response_time'].isoformat() + 'Z'),
             ]
-            field:list = [
+            field: list = [
                 'url',
                 'response_time',
-                #'domain',
+                # 'domain',
             ]
 
-            solr_records_temp: Any = solr_news_clip.search_query(search_query=solr_query,field=field)
+            solr_records_temp: Any = solr_news_clip.search_query(
+                search_query=solr_query, field=field)
 
             # news_clip_master側に存在しないcrawler_responseがある場合
             if solr_records_temp:
-                solr_records:pysolr.Results = solr_records_temp
+                solr_records: pysolr.Results = solr_records_temp
                 # crawler_responseとnews_clip_masterで同期している場合、同期リストへ保存
                 # ※通常1件しかないはずだが、障害によりリカバリした場合などは複数件存在する可能性がある。
                 for solr_record in solr_records:
@@ -276,7 +285,8 @@ def check(kwargs: dict):
     # スクレイピングミス分のurlがあれば
     if len(master_async_list) > 0:
         # メール通知用メッセージ追記
-        message = message + '以下のドメインでcrawler_responseにあるにもかかわらず、news_clip_master側に登録されていないケースがあります。\n'
+        message = message + \
+            '以下のドメインでcrawler_responseにあるにもかかわらず、news_clip_master側に登録されていないケースがあります。\n'
         for item in master_async_domain_aggregate.items():
             if item[1] > 0:
                 message = message + item[0] + ' : ' + str(item[1]) + ' 件\n'
