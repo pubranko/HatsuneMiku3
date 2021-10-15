caaee97269c64c1ba1eaa09750246a351492812f
pubranko <mikuras3@outlook.com>
2021-10-15 13:30:41 +0900

    scrapying_flowに後続実施機能を実装。mongoのfindにindex作成機能を実装。

--- models/scraped_from_response_model.py
+++ models/scraped_from_response_model.py
@@ -8,24 +8,3 @@ class ScrapedFromResponseModel(MongoCommonModel):
     '''
     mongo: MongoModel
     collection_name: str = 'scraped_from_response'
-
-# class ScrapedFromResponse(object):
-#     '''
-#     scraped_from_responseコレクション用モデル
-#     '''
-#     mongo: MongoModel
-
-#     def __init__(self, mongo: MongoModel):
-#         self.mongo = mongo
-
-#     def insert_one(self, item:dict):
-#         self.mongo.mongo_db['scraped_from_response'].insert_one(item)
-
-#     def insert(self, items:list):
-#         self.mongo.mongo_db['scraped_from_response'].insert(items)
-
-#     def find_one(self, projection=None,filter=None):
-#         return self.mongo.mongo_db['scraped_from_response'].find_one(projection=projection,filter=filter)
-
-#     def find(self, projection=None,filter=None, sort=None):
-#         return self.mongo.mongo_db['scraped_from_response'].find(projection=projection,filter=filter,sort=sort)
