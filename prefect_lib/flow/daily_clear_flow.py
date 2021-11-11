5a5ad65378407f3c221ca7b278da01043bf33d1b
pubranko <mikuras3@outlook.com>
2021-11-11 23:57:15 +0900

    エクスポートファイルの権限を読み取り専用に。日次削除機能実装完了。

---
+++ prefect_lib/flow/daily_clear_flow.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import logging
+from datetime import datetime
+from prefect import Flow, task, Parameter
+from prefect.core.parameter import DateTimeParameter
+from prefect.tasks.control_flow.conditional import ifelse
+from prefect.engine import signals
+from prefect.utilities.context import Context
+path = os.getcwd()
+sys.path.append(path)
+from prefect_lib.task.daily_clear_task import DailyClearTask
+from prefect_lib.settings import TIMEZONE
+from prefect_lib.common_module.flow_status_change import flow_status_change
+
+start_time = datetime.now().astimezone(TIMEZONE)
+log_file_path = os.path.join(
+    'logs', os.path.splitext(os.path.basename(__file__))[0] + '.log')
+logging.basicConfig(level=logging.DEBUG, filemode="w+", filename=log_file_path,
+                    format='%(asctime)s %(levelname)s [%(name)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+with Flow(
+    name='Mongo import selector flow',
+    state_handlers=[flow_status_change],
+) as flow:
+    task = DailyClearTask(
+        log_file_path=log_file_path, start_time=start_time)
+    result = task()
+
+flow.run()
