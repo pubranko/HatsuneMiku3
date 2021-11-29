fb988c06647c61202b24432fb956a33f761f9ece
pubranko <mikuras3@outlook.com>
2021-11-29 22:22:00 +0900

    大紀元のスクレイパー修正

--- prefect_lib/flow/scrapying_flow.py
+++ prefect_lib/flow/scrapying_flow.py
@@ -41,9 +41,9 @@ with Flow(
 
 # domain、crawling_start_time_*による絞り込みは任意
 flow.run(parameters=dict(
-    #domain='sankei.com',
-    crawling_start_time_from=datetime(2021, 10, 15, 13, 0, 0, 286000).astimezone(TIMEZONE),
+    domain='epochtimes.jp',
+    crawling_start_time_from=datetime(2021, 11, 29, 20, 0, 0, 0).astimezone(TIMEZONE),
     #crawling_start_time_to=datetime(2021, 9, 25, 11, 8, 28, 286000).astimezone(TIMEZONE),
     #urls=['https://www.sankei.com/article/20210829-2QFVABFPMVIBNHSINK6TBYWEXE/?outputType=theme_tokyo2020',]
-    #following_processing_execution='Yes',    # 後続処理実行(news_clip_masterへの登録,solrへの登録)
+    following_processing_execution='Yes',    # 後続処理実行(news_clip_masterへの登録,solrへの登録)
 ))
--- prefect_lib/scraper/epochtimes_jp.py
+++ prefect_lib/scraper/epochtimes_jp.py
@@ -10,6 +10,7 @@ from bs4.element import ResultSet
 from datetime import datetime
 from dateutil.parser import parse
 from prefect_lib.settings import TIMEZONE
+from common_lib.timezone_recovery import timezone_recovery
 
 file_name = os.path.splitext(os.path.basename(__file__))[0]
 logger: Logger = logging.getLogger('prefect.scraper.' + file_name)
@@ -35,17 +36,25 @@ def exec(record: dict, kwargs: dict) -> dict:
 
     # article
     article_selecter: Any = soup.select('article .page_content')
+    if not article_selecter:
+        article_selecter: Any = soup.select(
+            '.middle_part > .text')
     if article_selecter:
         result_set: ResultSet = article_selecter
         tag: Tag = result_set[0]
         scraped_record['article'] = tag.get_text().strip()
 
     # publish_date
+    publish_selecter: Any = soup.select_one(
+        'head > meta[name="pubdate"]')
+
     publish_selecter: Any = soup.select_one('.page_datetime.col-sm-12.col-md-12')
     if publish_selecter:
         tag: Tag = publish_selecter
         scraped_record['publish_date'] = datetime.strptime(tag.get_text().replace(
             '\n', '').strip(), '%Y年%m月%d日 %H時%M分').astimezone(TIMEZONE)
+    else:
+        scraped_record['publish_date'] = timezone_recovery(record['source_of_information']['lastmod'])
 
     # 発行者
     scraped_record['issuer'] = ['大紀元時報日本',
